e5ab9d57918d3327f9b051b30c6f69a8a1b05611
Jordan8899 <burmei.jorda@awatapu.school.nz>
2022-02-24 09:18:55 +1300

    higher lower 100% working

--- 06_higher_lower.py
+++ 06_higher_lower.py
@@ -1,22 +1,30 @@
+# Variables
 import random
 number = random.randint(1, 10)
 guess_amount = 1
 code_exit = False
 
-while guess_amount != 4 and code_exit == False:
-    guess = int(input("Guess a number between 1 and 10\n"))
+# Main Routine
+while guess_amount != 4 and not code_exit:
+    try:
+        guess = int(input("Guess a number between 1 and 10\n"))
 
-    if guess != number:
-        guess_amount = guess_amount + 1
+        if guess != number:
+            guess_amount = guess_amount + 1
 
-        if guess > number:
-            print("Incorrect, it's lower\n")
-        elif guess < number:
-            print("Incorrect, it's higher\n")
+            if guess > number:
+                print("Incorrect, it's lower\n")
+            elif guess < number:
+                print("Incorrect, it's higher\n")
 
-    elif guess == number:
-        print("Correct\n")
-        code_exit = True
+        elif guess == number:
+            print("Correct\n")
+            code_exit = True
+
+        else:
+            print("Please input int")
+    except ValueError:
+        print("Please input int")
 
 if guess_amount == 4 and guess != number:
     print("You have run out of guesses the number was {}".format(number))
