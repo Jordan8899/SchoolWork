07d4b98a2f8128ceb41a99a29e9f2f4257e01621
Jordan8899 <86272781+Jordan8899@users.noreply.github.com>
2022-02-28 11:41:48 +1300

    Update 06_number_guess.py

--- 06_number_guess.py
+++ 06_number_guess.py
@@ -5,16 +5,21 @@ guess_amount = 1
 code_exit = False
 
 while guess_amount != 4 and code_exit == False:
-    guess = int(input("Guess a number between 1 and 10\n"))
-    if guess != random_number:
-        print("Incorrect")
-        guess_amount = guess_amount + 1
-    elif guess == random_number:
-        print("Correct")
-        code_exit = True
+    try:
+        guess = int(input("Guess a number between 1 and 10\n"))
+        if guess != random_number:
+            print("Incorrect")
+            guess_amount = guess_amount + 1
+        elif guess == random_number:
+            print("Correct")
+            code_exit = True
+    
+    except ValueError:
+        print("Error")
+
 
 if guess_amount == 4 and guess != random_number:
-    print("You have run out of guesses the number was {}".format(random_number))
+   print("You have run out of guesses the number was {}".format(random_number))
 
 elif guess == number:
-    print("Good job you guessed the number in {} guess/es".format(guess_amount))
+   print("Good job you guessed the number in {} guess/es".format(guess_amount))
